f08acfc77d050a2822ff750caa204f0e6304548d
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2025-11-12 21:58:35 +0700

    done all heroes endpoint

--- src/main/kotlin/plugins/Routing.kt
+++ src/main/kotlin/plugins/Routing.kt
@@ -2,6 +2,7 @@ package com.example.plugins
 
 import com.example.routes.getAllHeroes
 import com.example.routes.root
+import com.example.routes.searchHeroes
 import io.ktor.server.application.*
 import io.ktor.server.http.content.resources
 import io.ktor.server.http.content.static
@@ -11,6 +12,7 @@ fun Application.configureRouting() {
     routing {
         root()
         getAllHeroes()
+        searchHeroes()
 
         static("/images"){
             resources("images")
