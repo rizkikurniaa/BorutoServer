c2cb302064300ab74b8ff1704535fa62321d75f8
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2025-11-12 21:46:28 +0700

    updated all heroes endpoint

--- src/main/kotlin/plugins/Routing.kt
+++ src/main/kotlin/plugins/Routing.kt
@@ -3,11 +3,17 @@ package com.example.plugins
 import com.example.routes.getAllHeroes
 import com.example.routes.root
 import io.ktor.server.application.*
+import io.ktor.server.http.content.resources
+import io.ktor.server.http.content.static
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
         root()
         getAllHeroes()
+
+        static("/images"){
+            resources("images")
+        }
     }
 }
